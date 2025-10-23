6c80dd7dc9f988538723d6c456faa7b7cef4cc4b
MarianaReis1 <marianaluizamr@gmail.com>
2025-10-23 13:05:32 +0100

    use github pages config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://marianareis1.github.io',
+  base: '/beabaencantos',
   vite: {
     plugins: [tailwindcss()]
   }
