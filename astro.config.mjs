733970d2a403098dae6763d50ee2122719ad5b5d
MarianaReis1 <marianaluizamr@gmail.com>
2025-10-23 17:55:26 +0100

    update site and remove base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://marianareis1.github.io',
-  base: 'https://www.beabaencantos.com.br/',
+  site: 'https://www.beabaencantos.com.br/',
   vite: {
     plugins: [tailwindcss()]
   }
